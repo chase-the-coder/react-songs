325586d428de038416c7df9dcfdd7c1d3c615447
Chase Lindsay <chase.lindsay1@gmail.com>
2020-04-16 17:51:22 -0400

    created actions and reducers

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { select } from '../actions';
+import { bindActionCreators } from 'redux';
+
+const App = () => {
+	return <div>App</div>;
+};
+
+export default App;
